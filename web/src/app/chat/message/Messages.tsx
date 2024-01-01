d8f276da20cd097c7b04e8babc5b919acb8982b0
Sid Ravinutala <sid.ravi1@gmail.com>
2024-01-01 18:46:58 +0000

    Switch HubGPT with Danswer for branding

--- web/src/app/chat/message/Messages.tsx
+++ web/src/app/chat/message/Messages.tsx
@@ -63,7 +63,7 @@ export const AIMessage = ({
               </div>
             </div>
 
-            <div className="font-bold text-emphasis ml-2 my-auto">Danswer</div>
+            <div className="font-bold text-emphasis ml-2 my-auto">HubGPT</div>
 
             {query === undefined &&
               hasDocs &&
