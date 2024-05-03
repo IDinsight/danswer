11427fed012b02919da8080951f761b052650f32
Mark Botterill <markbotters@gmail.com>
2024-05-03 17:11:37 +0000

    Add graphic ability to metric reporting

--- backend/scripts/send_slack_report/queries.py
+++ backend/scripts/send_slack_report/queries.py
@@ -1,27 +1,77 @@
 # This script stores all SQL queries which are used
 # to get HubGPT usage stats
 
-SLACK_MESSAGES_QUERY = """SELECT COUNT(*)
-    FROM chat_session
-    WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') -
-    INTERVAL '7 days')
-    AND user_id IS NULL
-"""
+MEDIUMS = ["slack_messages", "web_messages", "distinct_web_users"]
 
-WEB_MESSAGES_QUERY = """SELECT COUNT(*)
-    FROM chat_session
-    WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') -
-    INTERVAL '7 days')
-    AND user_id IS NOT NULL
-"""
 
-WEB_USERS_QUERY = """
-    SELECT COUNT(DISTINCT user_id)
-    FROM chat_session
-    WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') -
-    INTERVAL '7 days')
-    AND user_id IS NOT NULL
-"""
+USAGE_QUERIES = {
+    "last_7_days": {
+        "slack_messages": """
+            SELECT COUNT(*)
+            FROM chat_session
+            WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') - INTERVAL '7 days')
+            AND user_id IS NULL
+        """,
+        "web_messages": """
+            SELECT COUNT(*)
+            FROM chat_session
+            WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') - INTERVAL '7 days')
+            AND user_id IS NOT NULL
+        """,
+        "distinct_web_users": """
+            SELECT COUNT(DISTINCT user_id)
+            FROM chat_session
+            WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') - INTERVAL '7 days')
+            AND user_id IS NOT NULL
+        """,
+    },
+    "day_14_to_7": {
+        "slack_messages": """
+            SELECT COUNT(*)
+            FROM chat_session
+            WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') - INTERVAL '14 days')
+            AND time_created < (NOW() AT TIME ZONE 'UTC') - INTERVAL '7 days'
+            AND user_id IS NULL
+        """,
+        "web_messages": """
+            SELECT COUNT(*)
+            FROM chat_session
+            WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') - INTERVAL '14 days')
+            AND time_created < (NOW() AT TIME ZONE 'UTC') - INTERVAL '7 days'
+            AND user_id IS NOT NULL
+        """,
+        "distinct_web_users": """
+            SELECT COUNT(DISTINCT user_id)
+            FROM chat_session
+            WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') - INTERVAL '14 days')
+            AND time_created < (NOW() AT TIME ZONE 'UTC') - INTERVAL '7 days'
+            AND user_id IS NOT NULL
+        """,
+    },
+    "day_35_to_28": {
+        "slack_messages": """
+            SELECT COUNT(*)
+            FROM chat_session
+            WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') - INTERVAL '35 days')
+            AND time_created < (NOW() AT TIME ZONE 'UTC') - INTERVAL '28 days'
+            AND user_id IS NULL
+        """,
+        "web_messages": """
+            SELECT COUNT(*)
+            FROM chat_session
+            WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') - INTERVAL '35 days')
+            AND time_created < (NOW() AT TIME ZONE 'UTC') - INTERVAL '28 days'
+            AND user_id IS NOT NULL
+        """,
+        "distinct_web_users": """
+            SELECT COUNT(DISTINCT user_id)
+            FROM chat_session
+            WHERE (time_created >= (NOW() AT TIME ZONE 'UTC') - INTERVAL '35 days')
+            AND time_created < (NOW() AT TIME ZONE 'UTC') - INTERVAL '28 days'
+            AND user_id IS NOT NULL
+        """,
+    },
+}
 INITIAL_MESSAGES_QUERY = """
     SELECT message as initial_query FROM (
         SELECT *,
